dafdbeddb8ccde79d8f60bc5f6e8ba812791f68b
berupon <berupon@b0114f3a-c26b-4925-a6c5-ba312b392160>
2010-02-14 06:42:31 +0000

    multi-pass対応開始。

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -42,6 +42,7 @@ int main(int argc, char* argv[])
 	unsigned char* pSrc = (unsigned char*) _mm_malloc(size, 64);
 	unsigned char* pDest = (unsigned char*) _mm_malloc(size, 64);
 	unsigned char* pWork = (unsigned char*) _mm_malloc(size*2, 64);
+	unsigned char* pWork2 = (unsigned char*) _mm_malloc(size*2, 64);
 	
 	unsigned char palettes[256 * 4];
 	ReadImageData(fo, pSrc, width, palettes);
@@ -55,17 +56,25 @@ int main(int argc, char* argv[])
 	GetSystemInfo(&si);
 	
 	const size_t nThreads = si.dwNumberOfProcessors;
+//	const size_t nThreads = 1;
 //	const size_t nThreads = 2;
 	Threads<blur_1b::Parameter> threads;
 	threads.SetUp(nThreads);
 	const size_t partSize = size / nThreads;
 	const size_t partHeight = height / nThreads;
 	
+	blur_1b::Parameter pCommon;
+	pCommon.width = width;
+	pCommon.height = partHeight;
+	pCommon.srcLineOffsetBytes =
+	pCommon.workLineOffsetBytes =
+	pCommon.destLineOffsetBytes = width;
+	pCommon.radius = 2;
+	pCommon.iterationCount = 5;
 	std::vector<blur_1b::Parameter> params(nThreads);
 	for (size_t i=0; i<nThreads; ++i) {
 		blur_1b::Parameter& p = params[i];
-		p.width = width;
-		p.height = partHeight;
+		p = pCommon;
 		if (i == nThreads - 1) {
 			p.height = height - partHeight * i;
 		}
@@ -73,28 +82,25 @@ int main(int argc, char* argv[])
 		p.bBottom = (i == nThreads-1);
 		p.pSrc = pSrc + i * partSize;
 		p.pWork = pWork + i * partSize * 2;
+		p.pWork2 = pWork2 + i * partSize * 2;
 		p.pDest = pDest + i * partSize;
-		p.srcLineOffsetBytes =
-		p.workLineOffsetBytes =
-		p.destLineOffsetBytes = width;
-		p.radius = 16;
 		p.pTotalLine = (int16_t*) _mm_malloc(width * sizeof(int16_t), 64);
 	}
 	typedef void (*BlurFuncPtr)(const blur_1b::Parameter& p);
 	BlurFuncPtr ptrs[] = {
-		//blur_1b::test_1,
+		blur_1b::test_1,
 		//blur_1b::test_2,
 		//blur_1b::test_3,
-		blur_1b::test_4,
-		blur_1b::test_5_h,
-		blur_1b::test_5_v,
-		blur_1b::test_5_h,
-		blur_1b::test_6_v,
-		blur_1b::test_7_h,
-		blur_1b::test_7_v,
-		blur_1b::test_8,
-		blur_1b::test_9,
-		blur_1b::test_10,
+		//blur_1b::test_4,
+		//blur_1b::test_5_h,
+		//blur_1b::test_5_v,
+		//blur_1b::test_5_h,
+		//blur_1b::test_6_v,
+		//blur_1b::test_7_h,
+		//blur_1b::test_7_v,
+		//blur_1b::test_8,
+		//blur_1b::test_9,
+		//blur_1b::test_10,
 	};
 	
 	Timer t;
@@ -102,7 +108,7 @@ int main(int argc, char* argv[])
 	for (size_t i=0; i<countof(ptrs); ++i) {
 		t.Start();
 		
-		for (size_t j=0; j<3; ++j) {
+		for (size_t j=0; j<1; ++j) {
 			threads.Start(ptrs[i], &params[0]);
 			threads.Join();
 		}
